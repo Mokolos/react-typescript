c8f7ba475f809ab2c1d1514ae4a6aeae2c3b2ce2
Tendry <lemospy@gmail.com>
2022-07-08 14:42:29 +0300

    create schema for login validation using yup

---
+++ src/validator/loginSchema.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as yup from 'yup';
+
+const loginSchema = yup.object({
+  email: yup
+    .string()
+    .required("Email is required")
+    .email(),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, 'Password is too short - should be 8 chars minimum.')
+    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+}).required();
+
+export default loginSchema;
