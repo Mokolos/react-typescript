ff5c329e39c8cdf4ca1dcfe7f28468afa9693876
Tendry <lemospy@gmail.com>
2022-07-15 12:24:35 +0300

    add regex for email and password

--- src/validator/loginSchema.ts
+++ src/validator/loginSchema.ts
@@ -4,12 +4,17 @@ const loginSchema = yup.object({
   email: yup
     .string()
     .required("Email is required")
-    .email(),
+    .matches(
+      /[\w#][\w\.\’+#](.[\w\\’#]+)\@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*(.[a-zA-Z]{2,20})$/, 
+      'Enter the right email'),
   password: yup
     .string()
     .required("Password is required")
-    .min(6, 'Password is too short - should be 8 chars minimum.')
-    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+    .min(8, 'Password is too short - should be 8 chars minimum.')
+    .matches(
+      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,32}$ /, 
+      'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+    )
 });
 
 export default loginSchema;
